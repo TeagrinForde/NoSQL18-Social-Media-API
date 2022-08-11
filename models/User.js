058d7afcf29d0bb821785682964ea4f28f3f16b4
Teagrin Forde <teagrinb@gmail.com>
2022-08-11 18:41:08 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,31 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, Types } = require("mongoose");
 
-const userSchema = new Schema(
-    {
-        userId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId,
-        },
-    }
-)
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  thoughts: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  friends: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
